702050056058b8993c4387fd4c1c4672e13a5896
Will Kahn-Greene <willkg@mozilla.com>
2022-01-04 17:09:07 -0500

    Fix "Running on" message
    When running Kent in a Docker container with it set to bind to 0.0.0.0
    (all addresses), werkzeug (unhelpfully in this case) uses the ip address
    in the printed message of the Docker container which is unhelpful.

--- src/kent/cli.py
+++ src/kent/cli.py
@@ -3,11 +3,28 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import socket
 
-from flask.cli import main as flask_main
+from flask.cli import cli as flask_cli
+from werkzeug import serving
 
 os.environ["FLASK_APP"] = "kent.app"
+os.environ["WERKZEUG_RUN_MAIN"] = "true"
+
+
+def mock_get_interface_ip(family):
+    # NOTE(willkg): we do this weird thing becauwe werkzeug tries really hard
+    # to be helpful when the host is 0.0.0.0 or something like that and if
+    # you're running it in a docker container, it picks up the ip address of
+    # the container which is really unhelpful.
+    if family == socket.AF_INET:
+        return "localhost"
+    if family == socket.AF_INET6:
+        return "::1"
+
+
+serving.get_interface_ip = mock_get_interface_ip
 
 
 def main():
-    flask_main()
+    flask_cli.main()
