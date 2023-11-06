9986ceed85c453801b78fe7d5950f613302762d9
Will Kahn-Greene <willkg@mozilla.com>
2023-11-06 12:50:42 -0500

    Enhance testpost to support different kinds of posts

--- src/kent/cli_testpost.py
+++ src/kent/cli_testpost.py
@@ -6,7 +6,7 @@ import argparse
 import sys
 
 try:
-    from sentry_sdk import init, capture_message
+    from sentry_sdk import init, capture_exception, capture_message
 except ImportError:
     print("You need to have sentry_sdk installed.")
     sys.exit(1)
@@ -17,14 +17,27 @@ def main():
     parser.add_argument(
         "--dsn", default="http://public@localhost:5000/1", help="SENTRY_DSN to use"
     )
+    parser.add_argument(
+        "kind", nargs="?", default="message", help="What kind of thing to post. ['message', 'error']",
+    )
 
     args = parser.parse_args()
 
     init(args.dsn)
 
-    capture_message("test error capture")
-    print(f"Message posted to: {args.dsn}")
+    if args.kind == "message":
+        capture_message("test error capture")
+        print(f"Message posted to: {args.dsn}")
+    elif args.kind == "error":
+        try:
+            raise Exception
+        except Exception as exc:
+            capture_exception(exc)
+        print(f"Error posted to: {args.dsn}")
+    else:
+        print(f"{args.kind!r} is not a valid kind.")
+        return 1
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
